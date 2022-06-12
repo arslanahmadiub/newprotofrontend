d96501f9361d5b5a643ddcdb70d7fef3a8afc305
Arslan Ahmad <arslanahmadiub@gmail.com>
2022-06-12 05:23:23 +0500

    Initial Setup

---
+++ src/app/navigations.js
@@ -0,0 +1,136 @@
+export const navigations = [
+    {
+        name: 'Dashboard',
+        path: '/dashboard',
+        icon: 'dashboard',
+    },
+    {
+        label: 'PAGES',
+        type: 'label',
+    },
+    {
+        name: 'Session/Auth',
+        icon: 'security',
+        children: [
+            {
+                name: 'Sign in',
+                iconText: 'SI',
+                path: '/',
+            },
+            {
+                name: 'Sign up',
+                iconText: 'SU',
+                path: '/signup',
+            },
+            {
+                name: 'Forgot Password',
+                iconText: 'FP',
+                path: '/forgot-password',
+            },
+            {
+                name: 'Error',
+                iconText: '404',
+                path: '/not-found',
+            },
+        ],
+    },
+    {
+        label: 'Components',
+        type: 'label',
+    },
+    {
+        name: 'Components',
+        icon: 'favorite',
+        badge: { value: '30+', color: 'secondary' },
+        children: [
+            {
+                name: 'Auto Complete',
+                path: '/material/autocomplete',
+                iconText: 'A',
+            },
+            {
+                name: 'Buttons',
+                path: '/material/buttons',
+                iconText: 'B',
+            },
+            {
+                name: 'Checkbox',
+                path: '/material/checkbox',
+                iconText: 'C',
+            },
+            {
+                name: 'Dialog',
+                path: '/material/dialog',
+                iconText: 'D',
+            },
+            {
+                name: 'Expansion Panel',
+                path: '/material/expansion-panel',
+                iconText: 'E',
+            },
+            {
+                name: 'Form',
+                path: '/material/form',
+                iconText: 'F',
+            },
+            {
+                name: 'Icons',
+                path: '/material/icons',
+                iconText: 'I',
+            },
+            {
+                name: 'Menu',
+                path: '/material/menu',
+                iconText: 'M',
+            },
+            {
+                name: 'Progress',
+                path: '/material/progress',
+                iconText: 'P',
+            },
+            {
+                name: 'Radio',
+                path: '/material/radio',
+                iconText: 'R',
+            },
+            {
+                name: 'Switch',
+                path: '/material/switch',
+                iconText: 'S',
+            },
+            {
+                name: 'Slider',
+                path: '/material/slider',
+                iconText: 'S',
+            },
+            {
+                name: 'Snackbar',
+                path: '/material/snackbar',
+                iconText: 'S',
+            },
+            {
+                name: 'Table',
+                path: '/material/table',
+                iconText: 'T',
+            },
+        ],
+    },
+    {
+        name: 'Charts',
+        icon: 'trending_up',
+
+        children: [
+            {
+                name: 'Echarts',
+                path: '/charts/echarts',
+                iconText: 'E',
+            },
+        ],
+    },
+    {
+        name: 'Documentation',
+        icon: 'launch',
+        type: 'extLink',
+        path: 'http://demos.ui-lib.com/matx-react-doc/',
+    },
+]
